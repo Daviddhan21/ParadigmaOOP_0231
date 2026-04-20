695b4908edac03742c85fb61fd18c364f0e31633
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-04-20 17:37:17 +0700

    Menambahkan method printData pada class Mahasiswa

--- introOOP.cpp
+++ introOOP.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    float nilai;
 
-
-    void printData() {
-        cout << "NIM: " << nim << endl;
-        cout << "Nama: " << nama << endl;
-        cout << "Nilai: " << nilai << endl;
+    
     }
 }; // batas class
 
