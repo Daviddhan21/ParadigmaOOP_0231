c387ed9481a09051c50fc4357e09723335eeaf8b
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-04-20 18:35:25 +0700

    first commit

---
+++ introOOP.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData() {
+        cout << "NIM: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
+}; // batas class
+
+int main() {
+    Mahasiswa mhs;
+    mhs.nim = 2026;
+    mhs.nama = "YonoKiwi";
+    mhs.nilai = 89.5;
+
+    mhs.printData();
+
+   
+}// batas main
